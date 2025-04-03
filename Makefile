1dc802215949821bb7c588c3dc912b7dc6151b06
Rund Al-kofahi <r177a953@ku.edu>
2025-04-03 10:21:18 -0500

    Updating hpp

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC := g++
 CFLAGS := -Wall
 
-matrix.exe: matrix.o
-	$(CC) $(CFLAGS) -o matrix.exe matrix.o
+matrix.exe: main.o
+	$(CC) $(CFLAGS) -o main.exe
 
-football.o: matrix.c matrix.h
-	$(CC) $(CFLAGS) -c matrix.c -o matrix.o
+matrix.o: main.cpp
+	$(CC) $(CFLAGS) -c main.cpp
--- matrix.hpp
+++ matrix.hpp
@@ -10,24 +10,115 @@
 template <typename T>
 class Matrix {
 public:
-    Matrix(std::size_t N);
-    Matrix(std::vector<std::vector<T>> nums);
 
-    static std::vector<Matrix> read_from_file(const std::string &filename);
+    Matrix(std::size_t N) : _N(N), nums(N, std::vector<T>(N, 0)) {}
 
-    Matrix<T> operator+(const Matrix<T> &rhs) const;
-    Matrix<T> operator*(const Matrix<T> &rhs) const;
-    void set_value(std::size_t i, std::size_t j, T n);
-    T get_value(std::size_t i, std::size_t j) const;
-    int get_size() const;
+    Matrix(std::vector<std::vector<T>> nums) : _N(nums.size()), nums(std::move(nums)) {}
+
+    static std::vector<Matrix<T>> read_from_file(const std::string &filename) {
+        std::ifstream file(filename);
+        if (!file) {
+            throw std::runtime_error("Unable to open file");
+        }
+        
+        std::size_t N;
+        file >> N;
+        
+        std::vector<std::vector<T>> matrix1(N, std::vector<T>(N));
+        std::vector<std::vector<T>> matrix2(N, std::vector<T>(N));
+        
+        for (std::size_t i = 0; i < N; ++i) {
+            for (std::size_t j = 0; j < N; ++j) {
+                file >> matrix1[i][j];
+            }
+        }
+        
+        for (std::size_t i = 0; i < N; ++i) {
+            for (std::size_t j = 0; j < N; ++j) {
+                file >> matrix2[i][j];
+            }
+        }
+        
+        return {Matrix(matrix1), Matrix(matrix2)};
+    }
+
+    Matrix<T> operator+(const Matrix &rhs) const {
+        if (_N != rhs._N) {
+            throw std::invalid_argument("Matrix dimensions must match for addition");
+        }
+        Matrix result(_N);
+        for (std::size_t i = 0; i < _N; ++i) {
+            for (std::size_t j = 0; j < _N; ++j) {
+                result.nums[i][j] = nums[i][j] + rhs.nums[i][j];
+            }
+        }
+        return result;
+    }
+    
+    Matrix<T> operator*(const Matrix &rhs) const {
+        if (_N != rhs._N) {
+            throw std::invalid_argument("Matrix dimensions must match for multiplication");
+        }
+        Matrix result(_N);
+        for (std::size_t i = 0; i < _N; ++i) {
+            for (std::size_t j = 0; j < _N; ++j) {
+                for (std::size_t k = 0; k < _N; ++k) {
+                    result.nums[i][j] += nums[i][k] * rhs.nums[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    void set_value(std::size_t i, std::size_t j, T n) {
+        if (i >= _N || j >= _N) {
+            throw std::out_of_range("Index out of range");
+        }
+        nums[i][j] = n;
+    }
+
+    T get_value(std::size_t i, std::size_t j) const {
+        if (i >= _N || j >= _N) {
+            throw std::out_of_range("Index out of range");
+        }
+        return nums[i][j];
+    }
+
     int get_size() const{
         return _N;
     }
 
-    T sum_diagonal_major() const;
-    T sum_diagonal_minor() const;
-    void swap_rows(std::size_t r1, std::size_t r2);
-    void swap_cols(std::size_t c1, std::size_t c2);
+    T sum_diagonal_major() const{
+        T sum = 0;
+        for (std::size_t i = 0; i < _N; ++i) {
+            sum += nums[i][i];
+        }
+        return sum;
+    }
+    
+    T sum_diagonal_minor() const{
+        T sum = 0;
+        for (std::size_t i = 0; i < _N; ++i) {
+            sum += nums[i][_N - i - 1];
+        }
+        return sum;
+    }
+
+    void swap_rows(std::size_t r1, std::size_t r2){
+        if (r1 >= _N || r2 >= _N) {
+            throw std::out_of_range("Row index out of range");
+        }
+        std::swap(nums[r1], nums[r2]);
+    }
+
+    void swap_cols(std::size_t c1, std::size_t c2){
+        if (c1 >= _N || c2 >= _N) {
+            throw std::out_of_range("Column index out of range");
+        }
+        for (std::size_t i = 0; i < _N; ++i) {
+            std::swap(nums[i][c1], nums[i][c2]);
+        }
+    }
 
     void print_matrix() const{
         for (const auto &row : nums) {
